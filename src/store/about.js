84ecc129961be666309265c07c3a16b179a31959
leejpsd <wndvy7690@nate.com>
2022-05-08 20:24:13 +0900

    Create project

---
+++ src/store/about.js
@@ -0,0 +1,11 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'leejungpyo',
+    email: 'wndvy7690@nate.com',
+    blog: 'blog',
+    phone: '+82-10-4615-7794',
+    image: 'https://heropy.blog/css/images/logo.png'
+
+  })
+}
